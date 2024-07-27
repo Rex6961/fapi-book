fb045662af22f7de7b3db2cb903490951fe71039
Rex6961 <aon777ar@gmail.com>
2024-07-27 00:48:01 -0300

    Created templates from Jinja. HTTPException blocks fixed in todo.py

--- fapi_book/model/model.py
+++ fapi_book/model/model.py
@@ -1,9 +1,14 @@
 from pydantic import BaseModel
+from fastapi import Form
 
 
 class Todo(BaseModel):
-    id: int
+    id: int = None
     item: str
+
+    @classmethod
+    def as_form(cls, item: str = Form(...)):
+        return cls(item=item)
 
     # Example of using model
     model_config = {
--- fapi_book/web/todo.py
+++ fapi_book/web/todo.py
@@ -1,42 +1,56 @@
-from fastapi import APIRouter, Path, HTTPException, status
+from pathlib import Path as Pathlib
+
+from fastapi import (
+    APIRouter, Path, 
+    HTTPException, status, 
+    Request, Depends
+    )
+from fastapi.templating import Jinja2Templates
 
 from ..model.model import Todo, TodoItem, TodoItems
 
+top = Pathlib(__file__).resolve().parents[1]
 
 todo_router = APIRouter()
 
 todo_list = []
 
+templates = Jinja2Templates(directory=f"{top}/templates/")
+
 # This method creates variable
 @todo_router.post("/todo", status_code=201)
-async def add_todo(todo: Todo) -> dict:
+async def add_todo(request: Request, todo: Todo = Depends(Todo.as_form)) -> dict:
+    todo.id = len(todo_list) + 1
     todo_list.append(todo)
-    return {"message": "Todo added successfully"}
-
-# # This method gets all variables
-# @todo_router.get("/todo")
-# async def retrieve_todos() -> dict:
-#     return {"todos": todo_list}
+    return templates.TemplateResponse("todo.html",
+                                    {
+                                        "request": request,
+                                        "todos": todo_list
+                                    })
 
 # This method get all variables into TodoItem
 @todo_router.get("/todo", response_model=TodoItems)
-async def retrieve_todo() -> dict:
-    return {
-        "todos": todo_list
-    }
+async def retrieve_todo(request: Request) -> dict:
+    return templates.TemplateResponse("todo.html",
+                                    {
+                                        "request": request,
+                                        "todos": todo_list
+                                    })
 
 # This method gets one variable
 @todo_router.get("/todo/{todo_id}")
-async def get_single_todo(todo_id: int = Path(..., title="The ID of the todo to retrieve.")) -> dict:
+async def get_single_todo(request: Request, todo_id: int = Path(..., title="The ID of the todo to retrieve.")) -> dict:
     for todo in todo_list:
         if todo.id == todo_id:
-            return {
-                "todo": todo
-            }
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Todo with supplied ID doesn't exist",
-        )
+            return templates.TemplateResponse("todo.html",
+                            {
+                                "request": request,
+                                "todo": todo
+                            })
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Todo with supplied ID doesn't exist",
+    )
     
 # This method updates todo_list variables
 @todo_router.put("/todo/{todo_id}")
@@ -48,10 +62,10 @@ async def update_todo(todo_data: TodoItem, todo_id: int =
             return {
                 "message": "Todo update successfully."
             }
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Todo with supplied ID doesn't exist",
-        )
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Todo with supplied ID doesn't exist",
+    )
 
 # This method deletes one variable
 @todo_router.delete("/todo/{todo_id}")
@@ -63,10 +77,10 @@ async def delete_single_todo(todo_id: int) -> dict:
             return {
                 "message": "Todo deleted successfully."
             }
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Todo  with supplied ID doesn't exist",
-        )
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Todo  with supplied ID doesn't exist",
+    )
 
 # This method deletes all variables
 @todo_router.delete("/todo")
